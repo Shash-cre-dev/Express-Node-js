b5a6592619c3b6994cb67e3361b93f0edfd7efd6
Shashank <shashwritescode@gmail.com>
2023-09-25 19:30:33 +0530

    NPM basics

---
+++ Second Try/Node/npm/app.js
@@ -0,0 +1,36 @@
+// NPM or Node Package Manager
+
+// It allows us to 
+// Reuse our own code in another project
+// Use code written by other developers
+// Share own code with other developers
+
+// NPM calls a reusable code as package
+// A package is a folder that contains a JS code
+// Other name for a packages - modules or dependencies
+// All three are used interchangeably
+
+// There is no quality control in NPM Registry
+// Anyone can publish anything
+
+// good packages have high number of downloads
+
+// When we install Node we also install npm
+// This provides an access to npm global commands
+
+// npm --version (npm --v)
+
+// We can install packages in two ways
+// local dependency - use it only in a particular project (like this)
+// npm i <packageName> (i or install)
+
+// global dependency - use it in any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
+
+// More used is local dependency with the arrival of npx
+
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
