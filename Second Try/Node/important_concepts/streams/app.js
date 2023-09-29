4d8cfaa2f362b31ad799e68e8ffd2966b1f5912a
Shashank <shashwritescode@gmail.com>
2023-09-29 19:20:33 +0530

    Streams

---
+++ Second Try/Node/important_concepts/streams/app.js
@@ -0,0 +1,16 @@
+// STREAMS 
+
+// They are used to read or write sequentially
+// Eg for big data files
+
+// Four Types of Streams
+// Writeable - used to write data sequentially
+// Readable - used to read data sequentially
+// Duplex - used to both read and write data sequentially
+// Transform - used to modify data while writing or reading sequentially
+
+// Streams extend events emitterclass
+// meaning events like data and error can be used on streams
+
+
+
---
+++ Second Try/Node/important_concepts/streams/readStream.js
@@ -0,0 +1,22 @@
+const { createReadStream } = require('fs')
+
+const stream = createReadStream('../content/big.txt')
+
+//event - data
+stream.on('data', (result) =>{
+    console.log(result)
+})
+
+// we read data in chunks of 64kb (default)
+// chunks will either be in string or buffer
+// we can also change the size of the chunks with highWaterMark property
+
+// last buffer - remainder
+// highWaterMark - control size
+// const stream = createReadStream('./content/big.txt', {highWaterMark: 90000}) //90Kb
+
+//  We can set up the encoding also
+// const stream = createReadStream('./content/big.txt', {encoding: 'utf8'})
+
+// error event
+stream.on('error', (err)=> console.log(err))
