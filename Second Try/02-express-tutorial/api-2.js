792d4ee82e339efbfcbfe7df16079425367a5a80
Shashank <shashwritescode@gmail.com>
2023-10-09 18:37:16 +0530

    Route & Query Params

--- Second Try/02-express-tutorial/api-2.js
+++ Second Try/02-express-tutorial/api-2.js
@@ -3,6 +3,7 @@ const app = express()
 
 // importing data from data.js
 const { products } = require('./data')
+const { startsWith } = require('lodash')
 
 app.get('/', (req, res) => {
     res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
@@ -10,9 +11,9 @@ app.get('/', (req, res) => {
 
 // with mapping - you can get specific chunks of data
 app.get('/api/products', (req, res) => {
-    const newProducts = products.map((product)=>{
-        const {id, name, image} = product;
-        return {id, name, image}
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product;
+        return { id, name, image }
     })
     res.json(newProducts)
 })
@@ -30,15 +31,16 @@ app.get('/api/products/:productID', (req, res) => {
     // console.log(req)
     // console.log(req.params)
 
-    const {productID} = req.params
+    const { productID } = req.params
     const singleProduct = products.find((product) => product.id === Number(productID))
-    if(!singleProduct){
+    if (!singleProduct) {
         return res.status(404).send('Product Does Not Exist')
     }
     return res.json(singleProduct)
 })
 
-// Route Params (:)
+// ROUTE PARAMS (:)
+
 // They are like Placeholders where user provides a data req and params and set up some kind of logic
 // request params will always come in a string, convert the data type if required
 
@@ -57,10 +59,30 @@ app.listen(5000, () => {
 })
 
 // QUERY STRING PARAMETERS (URL PARAMETERS) (?)
+
 // Send small amount of info to the servers using url, like for db or filtering db
 
-app.get('/api/v1/query', (req,res)=>{
-    console.log(req.query)
-    res.send('Hello World')
+// for search and limits of the Products displayed at once
+app.get('/api/v1/query', (req, res) => {
+    // console.log(req.query)
+    const { search, limit } = req.query
+    let sortedProducts = [...products]
+
+    if (search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name, startsWith(search)
+        })
+    }
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    // no product found
+    if (sortedProducts.length < 1) {
+        // res.status(200).send('No Products that Matched your Search')
+        return res.status(200).json({ success: true, data: [] })
+    }
+    res.status(200).json(sortedProducts)
+    // res.send('Hello World')
 })
+
 // you can add as many query string param as you want
